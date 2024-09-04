b299f9de89b99d53a0a799bbe85bc833ee243bca
dmadronerowest <david.madronero@west.net.co>
2024-09-03 19:30:10 -0500

    Screen values update

--- ProjectFiles/NetSolution/DesignTimeNetLogic1.cs
+++ ProjectFiles/NetSolution/DesignTimeNetLogic1.cs
@@ -2,17 +2,17 @@
 using System;
 using UAManagedCore;
 using OpcUa = UAManagedCore.OpcUa;
-using FTOptix.SQLiteStore;
 using FTOptix.HMIProject;
+using FTOptix.NetLogic;
 using FTOptix.UI;
 using FTOptix.DataLogger;
 using FTOptix.NativeUI;
 using FTOptix.WebUI;
+using FTOptix.SQLiteStore;
 using FTOptix.Store;
 using FTOptix.RAEtherNetIP;
 using FTOptix.Retentivity;
 using FTOptix.CoreBase;
-using FTOptix.NetLogic;
 using FTOptix.CommunicationDriver;
 using FTOptix.EventLogger;
 using FTOptix.Alarm;
@@ -22,14 +22,8 @@ using FTOptix.Core;
 public class DesignTimeNetLogic1 : BaseNetLogic
 {
     [ExportMethod]
-    public void InsertBaseData()
+    public void Method1()
     {
-        Store myStore = Project.Current.Get<Store>("DataStores/MainDb");
-        Table myTable = myStore.Tables.Get<Table>("loops");
-        object[,] rawValues = new object[1, 2];
-        rawValues[0, 0] = "LOOP_001";
-        rawValues[0, 1] = "LOOP_001";
-        string[] columns = new string[2] { "Name", "LoopId"};
-        myTable.Insert(columns, rawValues);
+        // Insert code to be executed by the method
     }
 }
